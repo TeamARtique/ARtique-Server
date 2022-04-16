4970e01097e7d6477a2d8ab1fb40aef58d94f72f
hwangJi-dev <melon2228@swu.ac.kr>
2022-04-17 03:00:07 +0900

    ✅ Chore: 카카오 로그인 API 에러 핸들링

--- functions/src/controller/kakaoController.ts
+++ functions/src/controller/kakaoController.ts
@@ -15,11 +15,16 @@ const util = require("../lib/util");
  *  @access Private
  */
 export default async (req: Request, res: Response) => {
-    let access_token = req.body.accessToken;
+    let access_token;
+    if (!req.body.refreshToken) {
+        // ⛔️ body의 값이 없을 때 BAD_REQUEST response
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
     const refresh_token = req.body.refreshToken;
     const jwtRefreshtoken = jwtHandlers.refresh();
 
     try {
+        // ✅ kakao REST API를 사용하여 token 갱신
         const newToken = await axios({
             method: 'POST',
             headers: {
@@ -32,30 +37,26 @@ export default async (req: Request, res: Response) => {
                 refresh_token: refresh_token
             })
         }); 
-
-        if (newToken.status != 200) {   
-            return res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_ERROR));
-        }
         access_token = newToken.data.access_token;
-        console.log("new access token:", access_token);
 
+        // ✅ kakao REST API를 사용하여 accessToken을 해독한 후 user 정보 받아오기
         const user = await axios({
             method: 'GET',
             url: 'https://kapi.kakao.com/v2/user/me',
             headers: {
                 Authorization: `Bearer ${access_token}`
             }
-        }); // user 정보 받아오기
+        }); 
         
         let client;
         client = await db.connect();
         const checkUser = await userService.findUserByEmail(client, user.data.kakao_account.email);
         if (!checkUser) {
-            //DB에 없는 유저는 새로 생성한 후 토큰 발급한다.
+            // ✅ DB에 없는 유저는 새로 생성한 후 토큰 발급
             const newUser = await userService.createUser(client, user.data.kakao_account.email, user.data.kakao_account.profile.nickname, user.data.kakao_account.profile.profile_image_url);
             const jwtAccessToken = jwtHandlers.access(newUser.id); 
         
-            jwtHandlers(newUser._id);
+            jwtHandlers(newUser.id);
             let signData = {
                 user: newUser,
                 token: jwtAccessToken,
@@ -64,8 +65,9 @@ export default async (req: Request, res: Response) => {
             }
             return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.CREATED_USER, signData));
         }
-        // DB에 이미 존재하는 유저는 토큰 발급 후 전달
-        const jwtToken = jwtHandlers.access(checkUser.id);
+        
+        // ✅ DB에 이미 존재하는 유저는 토큰 발급 후 전달
+        const jwtToken = jwtHandlers.access(checkUser);
         userService.updateRefreshToken(client, checkUser.id, jwtRefreshtoken.refreshtoken);
         let loginData = {
             user: checkUser,
@@ -73,8 +75,15 @@ export default async (req: Request, res: Response) => {
             refreshToken: jwtRefreshtoken.refreshtoken
         }
         return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, loginData));
-    } catch (error) {
-        console.log(error);
+    } catch (error: any) {
+        // ⛔️ client측에서 넘어온 refreshToken이 유효하지 않을 경우
+        if (error.response) {
+            if (error.response.status === 400) {
+                return res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_ERROR));
+            }
+        }
+
+        // ⛔️ 내부 서버 오류
         return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
     }
 };
