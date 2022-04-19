5dbd487091c803fe35bfae6a5cc3ef2c4e091f06
hwangJi-dev <melon2228@swu.ac.kr>
2022-04-20 04:46:45 +0900

    ✨ Feat: (카테고리별) 홈 전시 리스트 조회 API 구현

--- functions/src/controller/exhibition/mainController.ts
+++ functions/src/controller/exhibition/mainController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import userService from "../../service/userService";
 import exhibitionService from "../../service/exhibitionService";
 const db = require('../../db/db');
 const responseMessage = require("../../constants/responseMessage");
@@ -11,18 +12,42 @@ const util = require("../../lib/util");
  *  @access Private
  */
 export default async (req: Request, res: Response) => {
-    let client;
+    let client: any;
     let category = parseInt(req.params.category);
     try {
         client = await db.connect(req);
         const categoryExhibitionList = await exhibitionService.getMainPostByCategory(client, category);
+        let popularExhibitionList = await exhibitionService.getMainPopularPostByCategory(client, category, req.body.user.id);
 
-        let mainData = {
-            forArtiExhibition: [],
-            popularExhibition: [],
+        let popularExhibitionPostList = await Promise.all(popularExhibitionList.map(async (popularData: any) => {
+            let artistData = await userService.findUserById(client, popularData.userId);
+            let data = {
+                exhibitionId: popularData.exhibitionId,
+                title: popularData.title,
+                posterImage: popularData.posterImage,
+                posterTheme: popularData.posterTheme,
+                artist: {
+                    artistId: popularData.userId,
+                    nickname: artistData.nickname,
+                },
+                like: {
+                    isLiked: popularData.isLiked == 1? true : false,
+                    likeCount: parseInt(popularData.likeCount)
+                },
+                bookmark: {
+                    isBookmarked: popularData.isBookmarked == 1? true : false,
+                    bookmarkCount: parseInt(popularData.bookmarkCount)
+                }
+            }
+            return data;
+        }));
+
+        let finalData = {
+            forArtiExhibition: popularExhibitionPostList,
+            popularExhibition: popularExhibitionPostList,
             categoryExhibition: [categoryExhibitionList]
         }
-        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_EXHIBITION_MAIN_SUCCESS, mainData));
+        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_EXHIBITION_MAIN_SUCCESS, finalData));
     } catch (error) {
         functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
         console.log(error);
--- functions/src/service/exhibitionService.ts
+++ functions/src/service/exhibitionService.ts
@@ -16,6 +16,38 @@ const getMainPostByCategory = async (client: any, category: number) => {
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
+// ✅ 카테고리별 메인뷰 인기 포스트 리스트 가져오기
+const getMainPopularPostByCategory = async (client: any, category: number, userId: number) => {
+  const { rows } = await client.query(
+    `
+    SELECT e.id as exhibition_id, e.title, e.poster_image, e.poster_theme, count(l.exhibition_id) as like_count, count(DISTINCT l2.user_id) as is_liked, e.user_id, count(b.exhibition_id) as bookmark_count, count(DISTINCT b2.user_id) as is_bookmarked
+    FROM "exhibition" e
+    INNER JOIN "like" l
+    ON e.id = l.exhibition_id
+    AND e.category = $1
+    AND e.is_deleted = false
+    LEFT OUTER JOIN "like" l2
+    ON e.id = l2.exhibition_id
+    AND e.category = $1
+    AND e.is_deleted = false
+    AND l2.user_id = $2
+    LEFT OUTER JOIN "bookmark" b
+    ON e.id = b.exhibition_id
+    AND e.category = $1
+    AND e.is_deleted = false
+    LEFT OUTER JOIN "bookmark" b2
+    ON e.id = b2.exhibition_id
+    AND e.category = $1
+    AND e.is_deleted = false
+    AND b2.user_id = $2
+    GROUP BY e.id ORDER BY count(l.exhibition_id) DESC LIMIT 6
+    `,
+    [category, userId]
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
+
 export default {
   getMainPostByCategory,
+  getMainPopularPostByCategory,
 };
