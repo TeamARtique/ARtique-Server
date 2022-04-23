8e9a636837c08f3c52bdb577cadf541bc69ce2bd
hwangJi-dev <melon2228@swu.ac.kr>
2022-04-24 03:50:57 +0900

    ✅ Chore: ResponseMessage 추가

--- functions/src/constants/responseMessage.ts
+++ functions/src/constants/responseMessage.ts
@@ -1,12 +1,14 @@
 module.exports = {
     // 오류
     NULL_VALUE: "필요한 값이 없습니다",
-    OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+    OUT_OF_VALUE: "요청한 값이 잘못되었습니다",
     INTERNAL_SERVER_ERROR: "서버 내부 오류",
     TOKEN_ERROR: "유효하지 않은 토큰입니다",
     TOKEN_EXPIRED: "만료된 토큰입니다",
     TOKEN_INVALID: "유효하지 않은 토큰입니다",
     TOKEN_EMPTY: "토큰이 없습니다",
+    INCORRECT_CATEGORY: "올바르지 않은 카테고리 아이디입니다.",
+    INCORRECT_SORT: "정렬값이 잘못되었습니다.",
 
     // 회원가입
     CREATED_USER: "회원 가입 성공",
@@ -22,7 +24,10 @@ module.exports = {
     READ_EXHIBITION_MAIN_SUCCESS: "카테고리별 홈 전시 리스트 조회 성공",
     READ_EXHIBITION_LIST_SUCCESS: "카테고리별 전시 리스트 조회 및 정렬 성공",
     READ_EXHIBITION_DETAIL_SUCCESS: "전세 상세 조회 성공",
-    READ_AR_GALLERY_DETAIL_SUCCESS: "전시관(AR) 상세조회 성공",
+    READ_AR_GALLERY_DETAIL_SUCCESS: "전시관(AR) 상세 조회 성공",
+
+    // 전시 수정
+    EXHIBITION_DETAIL_UPDATE_SUCCESS: "전시글 수정 성공",
 
     // 프로필 조회
     READ_PROFILE_SUCCESS: "프로필 조회 성공",
